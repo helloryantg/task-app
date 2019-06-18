9ca74e928ce84598d9220d8ac49d8f359c2c47c5
helloryantg <helloryantg@gmail.com>
2019-06-17 18:46:14 -0700

    Completes client routes for user and token service

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,2 +1,35 @@
 import axios from 'axios'
+import tokenService from './token.service'
 
+function signup(user) {
+    return axios.post('/api/users/signup', JSON.stringify(user))
+        .then(res => {
+            if (res.ok) return res.json()
+            throw new Error('Email already taken!')
+        })
+        .then(({ token }) => tokenService.setToken(token))
+}
+
+function login(creds) {
+    return axios.post('/api/users/login', JSON.stringify(creds))
+        .then(res => {
+            if (res.ok) return res.json()
+            throw new Error('Bad credentials')
+        })
+        .then(({ token }) => tokenService.setToken(token))
+}
+
+function getUser() {
+    return tokenService.getUserFromToken()
+}
+
+function logout() {
+    tokenService.removeToken
+}
+
+export default {
+    signup,
+    getUser,
+    logout,
+    login
+}
