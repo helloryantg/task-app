6fe80b4bca8678766f105f877e5dd9e954fcc7dd
helloryantg <helloryantg@gmail.com>
2019-06-17 18:36:33 -0700

    Adds token service

--- server/routes/api/user.route.js
+++ server/routes/api/user.route.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../../models/User.model')
 const userController = require('../../controllers/user.controller')
 
 router.post('/signup', userController.signup)
