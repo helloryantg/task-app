5b730292d485654166c6b214530a80719f273270
helloryantg <helloryantg@gmail.com>
2019-06-17 18:22:42 -0700

    Adds auth in config

---
+++ server/config/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+const SECRET = process.env.SECRET
+
+module.exports = (req, res, next) => {
+    const token = req.get('Authorization') || req.query.token || req.body.token
+
+    if (token) {
+        token = token.replace('Bearer ', '')
+        jwt.verify(token, SECRET, (err, decoded) => {
+            if (err) {
+                next(err)
+            } else {
+                req.user = decoded.user
+                next()
+            }
+        })
+    } else {
+        next()
+    }
+}
