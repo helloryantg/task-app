dc02d1949567dcb96d1aaf7162e4e8762b617bb2
Ryan Gonzales <rgonzales@avtr.com>
2019-10-04 14:46:00 -0700

    Adds styled components

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,66 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route
+    BrowserRouter as Router,
+    Switch,
+    Route
 } from 'react-router-dom'
-import './App.scss';
 import TaskWorkspace from '../TaskWorkspace'
 import LoginPage from '../LoginPage'
 import SignupPage from '../SignupPage'
 import * as groupServices from '../../services/group.service'
 import * as userServices from '../../services/user.service'
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
 
 class App extends Component {
-
-  state = {
-    user: null,
-    groups: null
-  }
-
-  componentDidMount() {
-    // const user = userServices.getUser()
-
-    // console.log('user', user)
-    
-    // groupServices.getAllGroups()
-    //   .then(({ data }) => {
-    //     this.setState(() => ({
-    //       groups: data
-    //     }))
-    //   })
-  }
-
-  handleChange = (e) => {
-    this.setState(() => ({
-      [e.target.name]: e.target.value
-    }))
-  }
-
-  handleSignupOrLogin = () => {
-    this.setState(() => ({
-      user: userServices.getUser()
-    }))
-  }
-
-  render() {
-
-    const { user, groups } = this.state
-
-    let body
-    if (!user) {
-      body = <LoginPage />
-    } else {
-      body = <TaskWorkspace groups={groups} />
+    state = {
+        user: null,
+        groups: null
     }
 
-    return (
-      <div className="App">
-        {body}
-      </div>
+    componentDidMount() {
 
-    )
-  }
+    }
+
+    handleChange = (e) => {
+        this.setState(() => ({
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    handleSignupOrLogin = () => {
+        this.setState(() => ({
+            user: userServices.getUser()
+        }))
+    }
+
+    render() {
+
+        const { user, groups } = this.state
+
+        let body
+        if (!user) {
+            // body = <LoginPage />
+        } else {
+            body = <TaskWorkspace groups={groups} />
+        }
+
+        return (
+            <AppWrapper>
+                {body}
+            </AppWrapper>
+        )
+    }
 }
 
-export default App;
+export default App
