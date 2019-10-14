14bc9c7c8c3ebf2c66cb033766d18a42522edcf4
Ryan Gonzales <helloryantg@gmail.com>
2019-10-14 12:15:21 -0700

    Cleans up Group controller

--- server/controllers/group.controller.js
+++ server/controllers/group.controller.js
@@ -4,47 +4,62 @@ const Group = require('../models/Group.model')
  * Creates a Group
  */
 const createGroup = (req, res, next) => {
-    const { label = '', owner = '' } = req.body
+    const { 
+        label = '', 
+        owner = '' 
+    } = req.body
 
     if (!label || !owner) {
-        return res.status(422).send({ error: 'You must provide a label and an owner' })
+        return res
+            .status(422)
+            .send({ error: 'You must provide a label and an owner' })
     }
 
-    Group.findOne({ owner, label }, function(err, existingGroup) {
-        if (err) { return next(err) }
+    Group.findOne({ owner, label }, function (err, existingGroup) {
+        if (err) return next(err)
 
         if (existingGroup) {
             // TODO - Add correct status
             return res.send({ error: "Label already exists, please use a different one" })
         }
-        
+
         const group = new Group({ label, owner })
 
-        group.save(function(err) {
-            if (err) { return next(err) }
+        group.save(function (err) {
+            if (err) return next(err) 
 
-            res.send(group._id)  
+            res.send(group._id)
         })
     })
 }
 
 /**
- * Get a Group
+ * Get a Group(s)
  */
 const getGroups = (req, res, next) => {
-    
-    const { owner = '', ids = [] } = req.body
+    const { 
+        owner = '', 
+        ids = [] 
+    } = req.body
 
     if (!owner) {
-        return res.status(422).send({ error: 'Please provide an owner'})
+        return res
+            .status(422)
+            .send({ error: 'Please provide an owner' })
     }
 
     if (!ids.length) {
-        return res.status(422).send({ error: 'You must provide at least one id' })
+        return res
+            .status(422)
+            .send({ error: 'You must provide at least one id' })
     }
 
-    Group.find().where('_id').in(ids).exec(function(err, existingGroups) {
-        if (err) { return next(err) }
+    Group
+        .find()
+        .where('_id')
+        .in(ids)
+        .exec(function (err, existingGroups) {
+        if (err) return next(err)
 
         if (!existingGroups.length) {
             res.send({ message: 'No groups found' })
@@ -58,15 +73,16 @@ const getGroups = (req, res, next) => {
  * Gets all of the user's groups
  */
 const getAllGroups = (req, res, next) => {
-
-    const { owner = ''} = req.query
+    const { owner = '' } = req.query
 
     if (!owner) {
-        return res.status(422).send({ error: 'Missing owner' })
+        return res
+            .status(422)
+            .send({ error: 'Missing owner' })
     }
 
-    Group.find({ owner }, function(err, groups) {
-        if (err) { return next(err) }
+    Group.find({ owner }, function (err, groups) {
+        if (err) return next(err)
 
         if (!groups.length) {
             return res.send({ message: 'This user has no groups' })
