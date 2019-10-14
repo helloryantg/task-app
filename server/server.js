404c0fa2a61f302b827d70a94b1c54a613f431c4
helloryantg <helloryantg@gmail.com>
2019-10-13 21:10:14 -0700

    Moves the package.json to server,.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
-const morgan = require('morgan')
 const logger = require('morgan')
 // const fs = require('fs')
 
@@ -19,7 +18,6 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(bodyParser.json({ type: '*/*' }))
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(morgan('combined'))
 
 // Routes
 // app.use(taskRouter)
