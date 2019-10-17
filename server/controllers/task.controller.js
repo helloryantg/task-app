7e38e44f59abc6c46d2b9d379cb44d880e5d0e99
Ryan Gonzales <helloryantg@gmail.com>
2019-10-16 18:45:20 -0700

    Fixes task routes and adds delete task operation

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -4,7 +4,7 @@ const Group = require('../models/Group.model')
 /**
  * Get User Tasks
  */
-const getTasks = (req, res, next) => {
+const getTasks = async (req, res, next) => {
     const user = req.user
 
     try {
@@ -18,7 +18,7 @@ const getTasks = (req, res, next) => {
 /**
  * Creates a Task
  */
-const createTask = (req, res, next) => {
+const createTask = async (req, res, next) => {
     const {
         description = '',
         groupName = '',
@@ -29,9 +29,7 @@ const createTask = (req, res, next) => {
     const user = req.user
 
     if (!groupName || !title) {
-        return res
-            .status(422)
-            .send({ error: 'You must provide a groupName and a task title' })
+        return res.status(422).send({ error: 'You must provide a groupName and a task title' })
     }
 
     Group.findOne({ _id: groupId }, function (err, existingGroup) {
@@ -45,15 +43,13 @@ const createTask = (req, res, next) => {
 /**
  * Updates a Task (INCOMPLETE CHECK THIS)
  */
-const updateTask = (req, res, next) => {
+const updateTask = async (req, res, next) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['title', 'description', 'groupName']
     const isValidOperation = updates.every(update => allowedUpdates.includes(update))
 
     if (!isValidOperation) {
-        return res
-            .status(400)
-            .send({ error: 'Invalid Updates!' })
+        return res.status(400).send({ error: 'Invalid Updates!' })
     }
 
     try {
@@ -71,18 +67,25 @@ const updateTask = (req, res, next) => {
         }
 
         res.send(task)
+
     } catch (e) {
-        return res
-            .status(400)
-            .send(e)
+        return res.status(400).send(e)
     }
 }
 
 /**
  * Deletes a Task
  */
-const deleteTask = (req, res, next) => {
+const deleteTask = async (req, res, next) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
 
+        if (!task) {
+            res.status(404).send()
+        }
+    } catch (e) {
+        return res.status(500).send()
+    }
 }
 
 module.exports = {
@@ -91,39 +94,3 @@ module.exports = {
     updateTask,
     deleteTask
 }
-
-// router.put('/tasks/:id', async (req, res) => {
-//     const updates = Object.keys(req.body)
-//     const allowedUpdates = ['title', 'description', 'groupName']
-//     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-//     if (!isValidOperation) {
-//         return res.status(400).send({ error: 'Invalid updates!'})
-//     }
-
-//     try {
-//         const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
-//         if (!task) {
-//             return res.status(404).send()
-//         }
-
-//         res.send(task)
-//     } catch (e) {
-//         res.status(400).send(e)
-//     }
-// })
-
-// router.delete('/tasks/:id', async (req, res) => {
-//     try {
-//         const task = await Task.findByIdAndDelete(req.params.id)
-
-//         if (!task) {
-//             res.status(404).send()
-//         }
-
-//         res.send(task)
-//     } catch (e) {
-//         res.status(500).send()
-//     }
-// })
--- server/routes/task.route.js
+++ server/routes/task.route.js
@@ -5,7 +5,7 @@ const taskController = require('../controllers/task.controller')
 
 router.get('/tasks', auth, taskController.getTasks)
 router.post('/tasks/create', auth, taskController.createTask)
-router.put('/tasks/update', auth, taskController.updateTask)
-router.delete('/tasks/delete', auth, taskController.deleteTask)
+router.put('/tasks/:id', auth, taskController.updateTask)
+router.delete('/tasks/:id', auth, taskController.deleteTask)
 
 module.exports = router
