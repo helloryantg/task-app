8742d61ec8043e08f4473b6fe684450e7b8238fa
Ryan Gonzales <helloryantg@gmail.com>
2019-10-16 18:32:41 -0700

    Moves task controller logic from router

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -28,3 +28,71 @@ const createTask = (req, res, next) => {
 
     const owner = req.user._id
 }
+
+// const Task = require('../models/Task.model')
+
+// router.get('/tasks', async (req, res) => {
+//     try {
+//         const tasks = await Task.find({})
+//         res.send(tasks)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
+
+// router.post('/tasks', async (req, res) => {
+//     const { 
+//         title,
+//         description,
+//         groupName
+//     } = req.body
+
+//     const task = new Task({
+//         title,
+//         description,
+//         groupName 
+//     })
+
+//     try {
+//         await task.save()
+//         res.status(201).send(task)
+//     } catch (e) {
+//         res.status(400).send(e)
+//     }
+// })
+
+// router.put('/tasks/:id', async (req, res) => {
+//     const updates = Object.keys(req.body)
+//     const allowedUpdates = ['title', 'description', 'groupName']
+//     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+//     if (!isValidOperation) {
+//         return res.status(400).send({ error: 'Invalid updates!'})
+//     }
+
+//     try {
+//         const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+//         if (!task) {
+//             return res.status(404).send()
+//         }
+
+//         res.send(task)
+//     } catch (e) {
+//         res.status(400).send(e)
+//     }
+// })
+
+// router.delete('/tasks/:id', async (req, res) => {
+//     try {
+//         const task = await Task.findByIdAndDelete(req.params.id)
+
+//         if (!task) {
+//             res.status(404).send()
+//         }
+
+//         res.send(task)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
--- server/routes/task.route.js
+++ server/routes/task.route.js
@@ -1,71 +1,11 @@
+const auth = require('../config/auth')
 const express = require('express')
 const router = express.Router()
-const Task = require('../models/Task.model')
+const taskController = require('../controllers/task.controller')
 
-router.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-router.post('/tasks', async (req, res) => {
-    const { 
-        title,
-        description,
-        groupName
-    } = req.body
-
-    const task = new Task({
-        title,
-        description,
-        groupName 
-    })
-
-    try {
-        await task.save()
-        res.status(201).send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-router.put('/tasks/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['title', 'description', 'groupName']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!'})
-    }
-
-    try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
-        if (!task) {
-            return res.status(404).send()
-        }
-
-        res.send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-router.delete('/tasks/:id', async (req, res) => {
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id)
-
-        if (!task) {
-            res.status(404).send()
-        }
-
-        res.send(task)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+router.get('/tasks', auth, taskController.getTasks)
+router.post('/tasks/create', auth, taskController.createTask)
+router.put('/tasks/update', auth, taskController.updateTask)
+router.delete('/tasks/delete', auth, taskController.deleteTask)
 
 module.exports = router
